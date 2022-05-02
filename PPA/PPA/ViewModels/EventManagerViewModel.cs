63c78eaf8ecb383f349c5b8d3ee4eac25c9109e5
Rachel Crowley <crowleyrr@gmail.com>
2022-05-02 16:52:38 -0400

    Added license info & small UI cleanup

--- PPA/PPA/ViewModels/EventManagerViewModel.cs
+++ PPA/PPA/ViewModels/EventManagerViewModel.cs
@@ -14,15 +14,47 @@ using System.Diagnostics;
 
 namespace PPA.ViewModels
 {
+    /*Calendar license info:
+     * 
+     * https://github.com/ME-MarvinE/XCalendar
+     * 
+     * MIT License
+
+        Copyright (c) 2022 MarvinE
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+     * 
+     */
+
+    /*
+     * Backend for Event Manager page
+     */
     public class EventManagerViewModel : CalendarBaseViewModel
     {
-        #region Properties
+        //Properties
         public ObservableRangeCollection<Event> Events { get; }
         public ObservableRangeCollection<DateTime> SelectedDates { get; }
         public ObservableRangeCollection<Event> SelectedEvents { get; }
 
         private bool _isBusy;
-        public bool IsBusy
+        public bool 
+            IsBusy
         {
             get
             {
@@ -34,15 +66,18 @@ namespace PPA.ViewModels
                 OnPropertyChanged("IsBusy");
             }
         }
-        #endregion
 
+        // Commands
         public AsyncCommand<Event> DeleteEventCommand { get; }
         public AsyncCommand AddEventCommand { get; }
         public AsyncCommand LoadEventsCommand { get; }
 
+        // Connect to database
         IEventDataStore EventService;
 
-        #region Constructors
+        /*
+         * Event Manager constructor
+         */
         public EventManagerViewModel()
         {
             AddEventCommand = new AsyncCommand(OnAddEvent);
@@ -59,19 +94,27 @@ namespace PPA.ViewModels
             SelectedDates.CollectionChanged += SelectedDates_CollectionChanged;
 
         }
-        #endregion
 
-        #region Methods
+        /*
+         * When a date is selected, add the events from that date 
+         * to the ones to be displayed
+         */
         private void SelectedDates_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             SelectedEvents.ReplaceRange(Events.Where(x => SelectedDates.Any(y => x.DateTime.Date == y.Date)).OrderByDescending(x => x.DateTime));
         }
 
+        /*
+         * Go to the New Event page
+         */
         private async Task OnAddEvent()
         {
             await Shell.Current.GoToAsync(nameof(NewEventPage));
         }
 
+        /*
+         * Load events from the datastore
+         */
         async Task LoadEvents()
         {
             IsBusy = true;
@@ -89,19 +132,24 @@ namespace PPA.ViewModels
         } 
         
 
+        /*
+         * Method for when Event manager page appears
+         */
         public async Task OnAppearing()
         {
             IsBusy = true;
             await LoadEvents();
         }
 
+        /*
+         * Deletes requested event based on ID
+         */
         private async Task OnDeleteEvent(Event ev)
         {
             await EventService.DeleteEventAsync(ev.Id);
             SelectedEvents.Remove(ev);
             await LoadEvents();
         }
-        #endregion
 
     }
 }
